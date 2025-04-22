c09cbd46b7ac0adf2f9d7c784c3caa4888da34d5
Caleb Abbott <cabbottdev@gmail.com>
2025-04-22 10:40:56 -0400

    cors

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import admin from 'firebase-admin';
-
+import cors from 'cors';
 admin.initializeApp();
 
 export const app = express();
+
+app.use(
+  cors({
+    origin: 'https://cha-shopping-list.web.app',
+  }),
+);
+
 export const db = admin.firestore();
