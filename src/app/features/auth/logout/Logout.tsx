ecba6cb33bd532a2937995a6381a92f045c5ca22
Caleb Abbott <cabbottdev@gmail.com>
2025-04-23 19:33:40 -0400

    clear state upon logout

--- src/app/features/auth/logout/Logout.tsx
+++ src/app/features/auth/logout/Logout.tsx
@@ -1,6 +1,3 @@
-// React
-import React from 'react';
-
 // Firebase
 import { signOut } from 'firebase/auth';
 import { auth } from '../../../../utility/firebase';
@@ -8,10 +5,20 @@ import { auth } from '../../../../utility/firebase';
 // Layout
 import { Button } from '../../../layout/Button';
 
+// Redux
+import { useDispatch } from 'react-redux';
+import { users } from '../../users/users-api';
+import { items } from '../../items/items-api';
+
 const Logout = (): React.JSX.Element => {
+  const dispatch = useDispatch();
+
   const handleLogout = async (): Promise<void> => {
     try {
       await signOut(auth);
+
+      dispatch(users.util.resetApiState());
+      dispatch(items.util.resetApiState());
     } catch (error) {
       console.error('Logout error:', error);
     }
