0480569f7e7ac46b9ec728d9715e5521fa6fcec7
Caleb Abbott <cabbottdev@gmail.com>
2025-04-24 14:06:37 -0400

    Try directly routing to /login after logging out

--- src/app/features/auth/logout/Logout.tsx
+++ src/app/features/auth/logout/Logout.tsx
@@ -7,18 +7,29 @@ import { Button } from '../../../layout/Button';
 
 // Redux
 import { useDispatch } from 'react-redux';
+
+// API Connections
 import { users } from '../../users/users-api';
 import { items } from '../../items/items-api';
 
+// React Router DOM
+import { Navigate, useLocation } from 'react-router-dom';
+
+// Routes
+import { route } from '../../../../routes/routes';
+
 const Logout = (): React.JSX.Element => {
   const dispatch = useDispatch();
+  const location = useLocation();
 
-  const handleLogout = async (): Promise<void> => {
+  const handleLogout = async (): Promise<React.JSX.Element | void> => {
     try {
       await signOut(auth);
 
-      dispatch(users.util.resetApiState());
       dispatch(items.util.resetApiState());
+      dispatch(users.util.resetApiState());
+
+      return <Navigate to={route.login} state={{ from: location }} replace />;
     } catch (error) {
       console.error('Logout error:', error);
     }
