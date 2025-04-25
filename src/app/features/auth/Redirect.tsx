df5e68bdeaf2732769da662498b6dd2961443f2f
Caleb Abbott <cabbottdev@gmail.com>
2025-04-25 12:28:01 -0400

    revert again

--- src/app/features/auth/Redirect.tsx
+++ src/app/features/auth/Redirect.tsx
@@ -1,44 +1,25 @@
 // React Router DOM
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 // Redux
-// import { RootState } from '../../store';
-// import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { useSelector } from 'react-redux';
 
 // Routes
 import { route } from '../../../routes/routes';
-
-// API Connection
-import { useGetAuthenticatedUserQuery } from '../users/users-api';
 
 type Props = {
   children: React.JSX.Element;
 };
 
 const Redirect = ({ children }: Props) => {
-  const location = useLocation();
-
-  const { data } = useGetAuthenticatedUserQuery();
-
-  // const authenticatedUser = useSelector(
-  //   (state: RootState) =>
-  //     state.users.queries['getAuthenticatedUser(undefined)']?.data
-  // );
-  // console.log('(Redirect) authenticatedUser:', authenticatedUser);
-
-  if (!data) {
-    sessionStorage.setItem('redirectAfterLogin', location.pathname);
-    return children;
-  }
-
-  const storedRedirect = sessionStorage.getItem('redirectAfterLogin');
-  sessionStorage.removeItem('redirectAfterLogin');
-
-  return data ? (
-    <Navigate to={storedRedirect || route.home} replace />
-  ) : (
-    children
+  const authenticatedUser = useSelector(
+    (state: RootState) =>
+      state.users.queries['getAuthenticatedUser(undefined)']?.data
   );
+  console.log('(Redirect) authenticatedUser:', authenticatedUser);
+
+  return authenticatedUser ? <Navigate to={route.home} replace /> : children;
 };
 
 export default Redirect;
